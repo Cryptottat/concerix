1d1dfa65f1fb4bdb76ce59026b53704d857a655e
cryptottat <cryptottat@users.noreply.github.com>
2025-11-20 12:31:41 +0900

    Add BUY  button to header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ function Header() {
           <a href="https://x.com/concerix" className="nav-link" data-text="x">x</a>
           <a href="https://docs.concerix.cloud" className="nav-link" data-text="docs">docs</a>
           <a href="javascript:void(0)" className="nav-link nav-link-disabled" data-text="app" onClick={(e) => { e.preventDefault(); e.stopPropagation(); }}>app</a>
+          <a href="https://pump.fun/coin/D6o1SzQwybAYvwW3NcF9u8rVZkXEj2RYsd2CKsKXpump" className="nav-link nav-link-buy" data-text="BUY $CCRX" target="_blank" rel="noopener noreferrer">BUY $CCRX</a>
         </nav>
       </div>
     </header>
